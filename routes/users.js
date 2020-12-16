19268089e71057d31157847294b4dca704fe2417
Viviana Garcia <vivianandrea4@gmail.com>
2020-12-16 16:07:20 -0500

    updated api

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-
-
 const User = require("../models/User");
 var jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
+
 // /auth/signup
-app.post("/signup", (req, res) => {
+app.post("/register", (req, res) => {
+    const emailExists = User.findOne({ email: req.body.email })
+    if (emailExists) {
+        return res.status(400).send({ message: "User already exists" })
+    }
     bcrypt.hash(req.body.password, 10, function (err, hash) {
         // Store hash in your password DB.
         if (err) console.log("HASING ERROR", err);
         else {
             User.create({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
                 email: req.body.email,
                 password: hash,
             })
                 .then((user) => {
-                    res.status(200).send("user created");
+                    res.status(200).send("User Created");
                 })
                 .catch((err) => {
                     res.status(500).send("Oooooeps");
@@ -28,24 +33,27 @@ app.post("/signup", (req, res) => {
 });
 // /auth/login
 app.post("/login", (req, res) => {
-
-    User.findOne({ email: req.body.email }).then((user) => {
-        if (!user) res.status(403).send("Invalid credentials");
-        else if (user) {
-            bcrypt.compare(req.body.password, user.password, function (err, match) {
-                // result == true
-                if (err) res.status(500).send("Oooooeps");
-                if (match) {
-                    var token = jwt.sign({ id: user._id }, "shhhhh");
-                    res.json({ token: token });
-                } else res.status(403).send("Invalid credentials");
-            });
-        } else {
-            res.status(403).send("Invalid credentials");
-        }
-    });
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) res.status(403).send("Invalid credentials");
+            else if (user) {
+                bcrypt.compare(req.body.password, user.password, function (err, match) {
+                    // result == true
+                    if (err) res.status(500).send("Oooooeps");
+                    if (match) {
+                        var token = jwt.sign({ id: user._id }, "shhhhh");
+                        res.json({ token: token });
+                    } else res.status(403).send("Invalid credentials");
+                });
+            } else {
+                res.status(403).send("Invalid credentials");
+            }
+        });
 });
 
 module.exports = app;
+
+
+
 
 
